b9ade63ae7b56ab8cee3c40ae386fa1fad783d6a
Alejandro Henkel <alehenkel17@gmail.com>
2016-10-17 13:58:43 -0500

    fix action buttons

--- client/templates/coordinator/groupsList/NewGroup.js
+++ client/templates/coordinator/groupsList/NewGroup.js
@@ -1,6 +1,6 @@
 Template.NewGroup.events({
 	// Closes new group mode when closing
-	'click .fa-close': function() {
+	'click .close': function() {
 		Session.set('newGroup', 0);
 	},
 	'click button[type="submit"]': function(event, template) {
