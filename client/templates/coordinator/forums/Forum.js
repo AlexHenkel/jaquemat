cc243934f1d452214492aa84959cd43ce55b18ec
Alejandro Henkel <alehenkel17@gmail.com>
2016-10-17 21:22:39 -0500

    fix forums and fix bugs

--- client/templates/coordinator/forums/Forum.js
+++ client/templates/coordinator/forums/Forum.js
@@ -9,23 +9,20 @@ Template.Forum.helpers({
 	},
 	editMode: function() {
 		return Template.instance().editMode.get();
-	},
-	console: function(i) {
-		console.log(i);
 	}
 });
 
 Template.Forum.events({
-	'click .fa-trash': function() {
+	'click .delete': function() {
 		Meteor.call('deleteForum', this._id);
 	},
-	'click .fa-pencil': function(event, template) {
+	'click .edit': function(event, template) {
 		template.editMode.set(!template.editMode.get());
 	},
 	'click button[type="submit"]': function(event, template) {
 		template.editMode.set(0);
 	}, 
-	'click .fa-close': function(event, template) {
+	'click .close': function(event, template) {
 		template.editMode.set(0);
 	}
 });
--- client/templates/coordinator/forums/NewForum.js
+++ client/templates/coordinator/forums/NewForum.js
@@ -1,6 +1,6 @@
 Template.NewForum.events({
 	// Closes new group mode when closing
-	'click .fa-close': function() {
+	'click .close': function() {
 		Session.set('newForum', 0);
 	},
 	'click button[type="submit"]': function(event, template) {
