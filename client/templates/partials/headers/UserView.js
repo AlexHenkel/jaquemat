c72a5e68b41c28d0cdbc5addf023846cc74de449
Alejandro Henkel <alehenkel17@gmail.com>
2016-10-18 13:40:28 -0500

    refactor helpers

--- client/templates/partials/headers/UserView.js
+++ client/templates/partials/headers/UserView.js
@@ -4,15 +4,3 @@ Template.UserView.onCreated(function () {
         self.subscribe('selfUser'); // Subscribe to self info
     });
 });
-
-Template.UserView.helpers({
-	name: function () {
-		return Meteor.user().extendedProfile.name;
-	},
-	email: function () {
-		return Meteor.user().extendedProfile.email;
-	},
-	profile_picture: function () {
-		return Meteor.user().extendedProfile.profile_picture;
-	}
-});
