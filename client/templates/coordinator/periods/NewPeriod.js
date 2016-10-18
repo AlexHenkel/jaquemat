70cdfd2ac516d7926a1041d27a6d85a49ebb9838
Alejandro Henkel <alehenkel17@gmail.com>
2016-10-17 21:15:16 -0500

    fix periods

--- client/templates/coordinator/periods/NewPeriod.js
+++ client/templates/coordinator/periods/NewPeriod.js
@@ -1,6 +1,6 @@
 Template.NewPeriod.events({
 	// Closes new group mode when closing
-	'click .fa-close': function() {
+	'click .close': function() {
 		Session.set('newPeriod', 0);
 	},
 	'click button[type="submit"]': function(event, template) {
