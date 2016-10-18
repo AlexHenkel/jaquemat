85f1e3466a7c5b5f3270626d42333187b3e6476d
Alejandro Henkel <alehenkel17@gmail.com>
2016-10-18 17:11:19 -0500

    adapt main template

--- client/templates/coordinator/singleGroup/SingleGroup.js
+++ client/templates/coordinator/singleGroup/SingleGroup.js
@@ -4,22 +4,22 @@ Template.SingleGroup.onCreated(function () {
     	let id = FlowRouter.getParam("id");
     	self.subscribe('singleGroup', id);
         self.subscribe('studentsInGroup', id);
-        self.subscribe('instructorsInGroup', id);
+    	self.subscribe('instructorsInGroup', id);
+    	self.subscribe('attendancesInGroup', id);
     });
-    this.editMode = new ReactiveVar(0);
 });
 
 Template.SingleGroup.helpers({
 	updateGroupId: function(){
 		return this._id;
-	},
-	editMode: function() {
-		return Template.instance().editMode.get();
 	}
 });
 
 Template.SingleGroup.events({
 	'click .edit': function(event, template) {
 		Session.set('editGroup', 1); // Open form to add groups
+	},
+	'click .attendance': function(event, template) {
+		Session.set('takeAttendance', 1); // Open form to add groups
 	}
 });
