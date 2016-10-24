6c9ef362e1dd6901925636b0512ddc1c2c629f8e
Alejandro Henkel <alehenkel17@gmail.com>
2016-10-24 11:03:26 -0500

    fix logout feature

--- client/templates/partials/headers/PersonalData.js
+++ client/templates/partials/headers/PersonalData.js
@@ -1,5 +1,6 @@
-Template.PersonalData.helpers({
+Template.PersonalData.events({
 	'click #logout': function(event) {
+		event.preventDefault();
         Meteor.logout(function(err){
             if (err) {
                 throw new Meteor.Error("Logout failed");
