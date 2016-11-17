b11d84e436b5fa07f07709e25c9302dc4b1f1f88
Alejandro Henkel <alehenkel17@gmail.com>
2016-11-16 20:29:14 -0600

    update fb ids

--- server/social-config.js
+++ server/social-config.js
@@ -4,6 +4,6 @@ ServiceConfiguration.configurations.remove({
  
 ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '1153367654701069',
-    secret: '4e28a0cb7c7b0ad0b19e23de1b42a93c'
+    appId: process.env.FB_APP_ID,
+    secret: process.env.FB_APP_SECRET
 });
